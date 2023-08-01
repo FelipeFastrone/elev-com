0d8bc7d1718d8ea354934414999a04dee32d4eac
Felipe_Fastrone <ffastrone@gmail.com>
2023-08-01 11:09:38 -0300

    commit

--- src/components/pages/Arcondicionado.jsx
+++ src/components/pages/Arcondicionado.jsx
@@ -4,7 +4,6 @@ import Button from "../btn/Button";
 import Container from "../layout/Container";
 
 
-import img from "../../assets/fundo2.jpeg"
 
 export default function arCondicionado(){
   
@@ -20,21 +19,32 @@ export default function arCondicionado(){
       <h4>Conheça nossas soluções</h4>
       <br></br>
       <div className={styles.wrapperCard}>
-       <div className={styles.card}></div>
-       <div className={styles.card}></div>
-       <div className={styles.card}></div>
-       <div className={styles.card}></div>
+       <div className={styles.card}>
+        <h4>Instalação</h4>
+        <p>Todas as nossas instalações são efetuadas de acordo com os procedimentos necessário para o perfeito funcionamento dos aparelhos de ar-condicionado . Realizamos todas as nossas instalações de acordo com o que determina o fabricante. Garantindo perfeito rendimento do aparelho e sem perder a garantia de fábrica.</p>
+       </div>
+       <div className={styles.card}><h4>MANUTENÇÃO CORRETIVA E CONSERTOS</h4>
+        <p>Contamos com uma equipe técnica altamente qualificada e devidamente treinada para melhor atender os nossos clientes. Garantindo assim uma análise precisa na hora de identificar o defeito do equipamento e na execução do serviço.</p></div>
+        </div>
+      <div className={styles.wrapperCard}>
+
+       <div className={styles.card}><h4>IMPLANTAÇÂO DE PMOC</h4>
+        <p>Trabalhamos com a implantação do PMOC para sua empresa. (PMOC: plano de manutenção operação e controle). Oferecendo todo o plano de manutenção e todos os laudos e documentos necessários de acordo com a lei. Junto com a implantação do PMOC oferecemos toda a assistência técnica com equipe altamente treinada e qualificada com todos os treinamentos e certificados necessários para atender a sua empresa. Proporcionando maior vida útil as peças e equipamentos de climatização da sua empresa, reduzindo custos com manutenções corretivas e consumo de energia .Trazendo também qualidade de ar para os seus funcionários. E evitando possíveis problemas com fiscalizações.
+
+       </p></div>
+       <div className={styles.card}><h4>HIGIENIZAÇÃO</h4>
+        <p>Efetuamos a higienização química do seu equipamento com ferramentas e produtos de alta qualidade, proporcionando uma limpeza completa no seu aparelho. Garantindo o funcionamento e vida útil do seu equipamento, e melhorando a qualidade de ar que você, sua família ou funcionários respiram.</p></div>
       </div>
+        </div>
+
+        </div>
       <Button 
        className={styles.btn}
        text="Faça um orçamento"
        />
-     </div>
 
      
-        <img className={styles.img} src={img} alt="Elestricista arrumando um poste"></img>
-     
-     </div>
+         
      </div>
    </Container>
   )
--- src/components/pages/Elevadores.jsx
+++ src/components/pages/Elevadores.jsx
@@ -15,28 +15,35 @@ export default function Elevadores(){
 
         <div className={styles.wrapper}>
       <h1 className={styles.title}>Elevadores!</h1>
-      <div className={styles.wrapperContent}>
-        <div className={styles.info}>
-      <h4>Consertos e novas instalações </h4>
-      <br></br>
-      <ul className={styles.list}>
-        <li>Automação de tarefas</li>
-        <li>Portões de garagem</li>
-        <li>Ventiladores de teto</li>
-        <li>Interruptores e tomadas</li>
-        <li>Chuveiros</li>
-        <li>Alarmes e cameras</li>
-      </ul>
+         <div className={styles.wrapperContent}>
+         <div className={styles.info}>
+        <h4>Consertos e novas instalações </h4>
+     <div className={styles.wrapperCard}>
+       <div className={styles.card}>
+        <h4>Instalação</h4>
+        <p>Todas as nossas instalações são efetuadas de acordo com os procedimentos necessário para o perfeito funcionamento dos aparelhos de ar-condicionado . Realizamos todas as nossas instalações de acordo com o que determina o fabricante. Garantindo perfeito rendimento do aparelho e sem perder a garantia de fábrica.</p>
+       </div>
+       <div> 
+        <img className={styles.img} src={img} alt="Elestricista arrumando um poste"></img>
+        </div>
+        </div>
+      <div className={styles.wrapperCard}>
+
+       <div> 
+        <img className={styles.img} src={img} alt="Elestricista arrumando um poste"></img>
+        </div>
+       <div className={styles.card}><h4>HIGIENIZAÇÃO</h4>
+        <p>Efetuamos a higienização química do seu equipamento com ferramentas e produtos de alta qualidade, proporcionando uma limpeza completa no seu aparelho. Garantindo o funcionamento e vida útil do seu equipamento, e melhorando a qualidade de ar que você, sua família ou funcionários respiram.</p></div>
+      </div>
+        </div>
+      </div>
       <Button 
        className={styles.btn}
        text="Faça um orçamento"
        />
-     </div>
 
      
-        <img className={styles.img} src={img} alt="Elestricista arrumando um poste"></img>
      
-     </div>
      </div>
    </Container>
   )
