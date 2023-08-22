77523b1f318ea07503bcc7e455ffeeeefd035b18
Felipe <ffastrone@yahoo.com>
2023-08-22 13:39:25 -0300

    commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const {resolve} = require('path')
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 const app = express();
 
 app.use('/', express.static(
