6b3b9b997253f312614f9d4823e00025ea267118
Felipe <ffastrone@yahoo.com>
2023-08-22 14:16:32 -0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 
-
+import { Route, Routes } from 'react-router-dom';
 import Home from './components/pages/Home';
 import EletricaResi from './components/pages/EletricaResi';
 import EnergiaSol from './components/pages/EnergiaSol';
 import Elevadores from './components/pages/Elevadores';
 import Arcondicionado from './components/pages/Arcondicionado';
-
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer'
+import Container from './components/layout/Container';
 
 
 
@@ -17,11 +19,33 @@ export default function App() {
 
   return (
     <div id="App">
-     <Home/>
-     <EnergiaSol/>
-     <EletricaResi/>
-     <Arcondicionado/>
-     <Elevadores/>
+     <Navbar />
+    <Container customClass="min-height">
+    <Routes>
+      <Route
+         path='/'
+         element={<Home/>}>
+      </Route>
+      <Route
+         path='/EletricaResi'
+         element={<EletricaResi/>}>
+      </Route>
+       <Route
+         path='/EnergiaSol'
+         element={<EnergiaSol/>}>
+       </Route>
+       <Route
+         path='/Elevadores'
+         element={<Elevadores/>}>
+       </Route>
+       <Route
+         path='/Arcondicionado'
+         element={<Arcondicionado/>}>
+       </Route>
+      </Routes>
+      </Container>
+      <Footer />
+    
     
       
     </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,54 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter} from 'react-router-dom';
 
 import App from './App.jsx';
 
-import Home from './components/pages/Home.jsx';
-import EletricaResi from './components/pages/EletricaResi.jsx';
-import EnergiaSol from './components/pages/EnergiaSol.jsx';
-import Elevadores from './components/pages/Elevadores.jsx';
-import ArCondicionado from './components/pages/Arcondicionado.jsx';
-
-import Navbar from "./components/layout/Navbar";
-import Container from './components/layout/Container.jsx';
 
 
 import './index.css';
-import Footer from './components/layout/Footer.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // eslint-disable-next-line react/jsx-no-comment-textnodes
   <React.StrictMode>
-    // eslint-disable-next-line no-undef, no-undef
     <BrowserRouter>
-    <Navbar />
-    <Container customClass="min-height">
-    <Routes
-       element={<App />}>
-      <Route
-         path='/'
-         element={<Home/>}exact >
-      </Route>
-      <Route
-         path='/EletricaResi'
-         element={<EletricaResi/>}>
-      </Route>
-       <Route
-         path='/EnergiaSol'
-         element={<EnergiaSol/>}>
-       </Route>
-       <Route
-         path='/Elevadores'
-         element={<Elevadores/>}>
-       </Route>
-       <Route
-         path='/Arcondicionado'
-         element={<ArCondicionado/>}>
-       </Route>
-      </Routes>
-      </Container>
-      <Footer />
+     <App/>
     </BrowserRouter>
   </React.StrictMode>,
 )
