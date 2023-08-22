d1ce29297d9010baaee8f46a653384fe7c6df4e5
Felipe <ffastrone@yahoo.com>
2023-08-22 09:00:12 -0300

    commit

---
+++ heroku/lib/commands/run/inside.d.ts
@@ -0,0 +1,15 @@
+import { Command } from '@heroku-cli/command';
+export default class RunInside extends Command {
+    static description: string;
+    static hidden: boolean;
+    static examples: string[];
+    static flags: {
+        app: import("@oclif/core/lib/interfaces").OptionFlag<string, import("@oclif/core/lib/interfaces/parser").CustomOptions>;
+        remote: import("@oclif/core/lib/interfaces").OptionFlag<string | undefined, import("@oclif/core/lib/interfaces/parser").CustomOptions>;
+        'exit-code': import("@oclif/core/lib/interfaces").BooleanFlag<boolean>;
+        env: import("@oclif/core/lib/interfaces").OptionFlag<string | undefined, import("@oclif/core/lib/interfaces/parser").CustomOptions>;
+        listen: import("@oclif/core/lib/interfaces").BooleanFlag<boolean>;
+    };
+    static strict: boolean;
+    run(): Promise<void>;
+}
